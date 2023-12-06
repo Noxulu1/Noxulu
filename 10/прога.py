b13a6db1dd98c40dc188e5a0c5aa4e590fb7be2e
Noxulu1 <67241474+Noxulu1@users.noreply.github.com>
2023-12-06 08:41:17 +0300

    прога.py

--- 10/прога.py
+++ 10/прога.py
@@ -1,25 +1,25 @@
-# Определите функцию для чтения матрицы из файла
+# Определяем функцию для чтения матрицы из файла
 def read_matrix(file_name):
     with open(file_name, 'r') as file:
         lines = file.readlines()
         matrix = [list(map(int, line.strip().split())) for line in lines]
     return matrix
 
-# Определите функцию для записи матрицы в файл
+# Определяем функцию для записи матрицы в файл
 def write_matrix(file_name, matrix):
     with open(file_name, 'w') as file:
         for row in matrix:
             file.write(' '.join(map(str, row)) + 'n')
 
-# Определите путь к файлу ввода и файлу вывода
+# Определяем путь к файлу ввода и файлу вывода
 input_file_name = 'C:/Пользователи/yakov/OneDrives/Яковишин Антон Максимович ПР10_УБ_31_vvod.txt'
 output_file_name = 'C:/Пользователи/yakov/OneDrives/Яковишин Антон Максимович ПР10_УБ_31_vivod.txt'
 
-# Считайте матрицу из файла ввода
+# Считаем матрицу из файла ввода
 matrix = read_matrix(input_file_name)
 
-# Выполните операции над матрицей (например, вычислите сумму элементов каждой строки)
+# Выполняем операции над матрицей (например, вычислите сумму элементов каждой строки)
 result_matrix = [[sum(row)] for row in matrix]
 
-# Запишите результаты в файл вывода
+# Записываем результаты в файл вывода
 write_matrix(output_file_name, result_matrix)
